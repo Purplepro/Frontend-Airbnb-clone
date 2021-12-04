e16d09b367047566a57f90d8080a3f43efbba19c
Sebastian G <gayle054@gmail.com>
2021-12-04 10:12:30 -0800

    preparing navbar

--- frontend-airbnb-clone/src/App.js
+++ frontend-airbnb-clone/src/App.js
@@ -1,9 +1,11 @@
 
 import './App.scss';
+import Navbar from './Components/A-Nav/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       
     </div>
   );
--- frontend-airbnb-clone/src/Components/A-Nav/Navbar.js
+++ frontend-airbnb-clone/src/Components/A-Nav/Navbar.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import RegularLogo from '../../Images/AirLogo.svg';
+import MobileLogo from '../../Images/RespAirLogo.svg';
+import { IoSearch } from 'react-icons/io5';
+
 
 function Navbar() {
+
+
+    const click = () => console.log('clicked')
+
     return (
         <div>
+            <div><img src={RegularLogo} alt="Airbnb logo" height="140px" width="140px"/></div>
+            <div className="SearchBar">
+            <h4>Start your search</h4>
+            <div role="button" aria-expanded="false" id="small-search" tabIndex={0} onClick={click}><IoSearch className="search-icon" /></div>
+            </div>
+            <div><a href="#">Places to go</a></div>
+            <div><a href="#">Experiences</a></div>
+            <div><a href="#">Online Experiences</a></div>
+            
             
         </div>
     )
